0b3347e7d2978ffaf0179b421d61d683e37da0ca
AShepelev <shepelev.as@phystech.edu>
2019-10-25 15:43:41 +0300

    created empty Swift project

---
+++ sprint_1_BlueTeam/sprint_1_BlueTeam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sprint_1_BlueTeam
+//
+//  Created by Aleksey Shepelev on 25/10/2019.
+//  Copyright © 2019 BlueTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
