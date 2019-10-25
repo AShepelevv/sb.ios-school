151f268703abf73193e3810fb4d6bcb0a35510b2
AShepelev <shepelev.as@phystech.edu>
2019-10-25 23:24:07 +0300

    removed sort methods and remove(by:) in ContainerProtocol, added removeAllElements() and getAnyElement() methods, method append(newElement:) renamed to addNewElement(_:). Also rewritten TeamContainer class.

--- sprint_1_BlueTeam/sprint_1_BlueTeam/ContainerProtocol/ContainerProtocol.swift
+++ sprint_1_BlueTeam/sprint_1_BlueTeam/ContainerProtocol/ContainerProtocol.swift
@@ -7,18 +7,21 @@
 //
 
 protocol ContainerProtocol {
-    
     associatedtype Element
     
     var isEmpty : Bool { get }
     var count : Int { get }
     var capacity : Int { get }
     
-    func append(_ newElement: Element)
-    
-    func remove(at: Int) -> Element
-    
-    func sort(by: (Element, Element) -> Bool)
-    
-    func sorted(by: (Element, Element) -> Bool) -> [Element]
+    func addNewElement(_ : Element)
+    func getAnyElement() throws -> Element
+    func removeAllElements(keepCapacity: Bool)
+}
+
+//  В статье на medium.com написано, что так можно
+//  параметры по умолчанию передавать
+extension ContainerProtocol {
+    func removeAllElements(keepCapacity: Bool = false) {
+        removeAllElements(keepCapacity: keepCapacity)
+    }
 }
--- sprint_1_BlueTeam/sprint_1_BlueTeam/ContainerProtocol/TeamContainer.swift
+++ sprint_1_BlueTeam/sprint_1_BlueTeam/ContainerProtocol/TeamContainer.swift
@@ -6,7 +6,13 @@
 //  Copyright © 2019 ashepelev. All rights reserved.
 //
 
+import Foundation
+
 class TeamContainer<T>: ContainerProtocol {
+ 
+    typealias Element = T
+    var container : Array<T>
+    
     var isEmpty: Bool {
         get {
             container.isEmpty
@@ -25,26 +31,19 @@ class TeamContainer<T>: ContainerProtocol {
         }
     }
     
-    func append(_ newElement: T) {
-        container.append(newElement)
-    }
-    
-    func remove(at: Int) -> T {
-        return container.remove(at: at)
-    }
-    
-    func sort(by: (T, T) -> Bool) {
-        container.sort(by: by)
-    }
-    
-    func sorted(by: (T, T) -> Bool) -> [T] {
-        return container.sorted(by: by)
-    }
-    
-    typealias Element = T
-    var container : Array<T>
-    
     init() {
         container = Array<T>()
+    }
+    
+    func addNewElement(_ newElement: T) {
+        container.append(newElement);
+    }
+    
+    func getAnyElement() throws -> T {
+        if count == 0 {
+            let exception = NSException()
+            exception.raise()
+        }
+        return container[0]
     }
 }
