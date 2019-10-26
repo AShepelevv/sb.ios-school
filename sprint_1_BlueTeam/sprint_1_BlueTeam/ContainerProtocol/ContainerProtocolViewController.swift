21af276759c060959aedd81c008862b5c207eb34
AShepelev <shepelev.as@phystech.edu>
2019-10-26 12:50:25 +0300

    added StoryBoard and project structure

---
+++ sprint_1_BlueTeam/sprint_1_BlueTeam/ContainerProtocol/ContainerProtocolViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContainerProtocolViewController.swift
+//  sprint_1_BlueTeam
+//
+//  Created by Aleksey Shepelev on 26/10/2019.
+//  Copyright © 2019 BlueTeam. All rights reserved.
+//
+
+import UIKit
+
+class ContainerProtocolViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
