ba3035afd60a2e92098071cd9ca52a3d3e0a1716
AShepelev <shepelev.as@phystech.edu>
2019-11-08 19:46:59 +0300

    added all files

---
+++ UIKitLesson/UIKitLesson/SecondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.swift
+//  UIKitLesson
+//
+//  Created by Kaplan Deniz on 11/10/2019.
+//  Copyright © 2019 DK. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		title = "Второй Controller"
+		view.backgroundColor = .white
+        
+        let nextButton = UIButton(type: .system)
+        nextButton.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        nextButton.center = view.center
+        nextButton.setTitle("Next", for: .normal)
+        nextButton.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
+        
+        view.addSubview(nextButton)
+	}
+    
+    @objc
+    func nextButtonTapped() {
+        let nextVC = ThirdViewController()
+        nextVC.modalPresentationStyle = .fullScreen
+        navigationController?.pushViewController(nextVC, animated: true)
+    }
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		self.navigationController?.setNavigationBarHidden(false, animated: animated)
+	}
+}
